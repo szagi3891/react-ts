6aab282757a9d89671e72e7ecc9be22aaefcf907
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2018-06-03 23:07:25 +0200

    mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
 
 interface PropsType {
     name: string
@@ -6,8 +8,10 @@ interface PropsType {
 
 class FF {
     name: string;
+    aa: number;
     constructor() {
-        this.name = "dadsada"
+        this.name = "dadsada";
+        this.aa = 2121;
     }
 }
 
@@ -15,17 +19,31 @@ const fff = (arg: number): number => {
     return arg + 1;
 };
 
+class RR {
+    @observable count: number;
+    constructor() {
+        this.count = 0;
+
+        setInterval(() => {
+            this.count++;
+        }, 1000);
+    }
+}
+
+const rr = new RR();
+
 //const tt: any = "312321";
 
 //fff(tt);
 //fff("31321a");
 
+@observer
 export class App extends React.Component<PropsType> {
     render() {
         const { name } = this.props;
 
         return (
-            <div> App komponent3 - { name } </div>
+            <div> App komponent3 - { name } - { rr.count } </div>
         );
     }
 }
